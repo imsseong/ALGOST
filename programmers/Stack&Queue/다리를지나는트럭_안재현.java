21c7c782c1247adb61a28ce1a233cc5febda84f5
jaeyooou <jaehyen2@naver.com>
2020-11-08 18:06:33 +0900

    Modify 기능개발 주식가격 다리를지나는트럭 안재현

--- programmers/Stack&Queue/다리를지나는트럭_안재현.java
+++ programmers/Stack&Queue/다리를지나는트럭_안재현.java
@@ -1,4 +1,37 @@
+import java.util.*;
 
-public class 다리를지나는트럭_안재현 {
-
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        int truck_len = truck_weights.length;
+        int weight_present = 0;
+        Queue<Integer> bridge = new LinkedList<>();
+        for(int truck : truck_weights){
+            while(true){
+                if(bridge.isEmpty()){ // 다리위에 아무것도 없으면
+                    bridge.add(truck);
+                    weight_present += truck;
+                    answer += 1;
+                    break;
+                }
+                else if(bridge.size() == bridge_length){ // 꽉 차 있으면
+                    weight_present -= bridge.poll();
+                }
+                else{
+                    if(weight_present + truck <= weight ){ // 꽉 차지 않고 다음트럭 들어올 수 있음
+                        bridge.add(truck);
+                        weight_present += truck;
+                        answer += 1;
+                        break;
+                    }
+                    else // 꽉 차지않았지만 무게때문에 다음트럭 올 수 없음
+                    {
+                        answer += 1;
+                        bridge.add(0);
+                    }
+                }
+            }
+        }
+        return answer + bridge_length;
+    }
 }
--- programmers/Stack&Queue/주식가격_안재현.java
+++ programmers/Stack&Queue/주식가격_안재현.java
@@ -1,4 +1,22 @@
-
-public class 주식가격_안재현 {
-
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = {};
+        int cnt = 0;
+        int length = prices.length;
+        answer = new int[length];
+        for(int i = 0 ; i < length ; i++){
+            for(int j = i+1 ; j < length ; j++){
+                if(prices[i] <= prices[j])
+                    cnt++;
+                else{
+                   cnt++;
+                    break;
+                }
+            }
+            answer[i] = cnt;
+            cnt = 0;
+            
+        }      
+        return answer;
+    }
 }
