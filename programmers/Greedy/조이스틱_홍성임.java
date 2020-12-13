6676a22fe49d247532a2badf4a5ec1fbb22b7edc
serena <serena@d2by.net>
2020-12-13 18:54:26 +0900

    Modify 조이스틱 홍성임

---
+++ programmers/Greedy/조이스틱_홍성임.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public int solution(String name) {
+        int size = name.length();
+
+        int upDown = 0;
+        for(int i = 0; i < size; i++) {
+            upDown += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+        }
+
+        int leftRight = name.length() - 1; //왼쪽 오른쪽 이동 횟수
+        for (int i = 0, j; i < size; i++) {
+            j = i + 1;
+            while(j < size && name.charAt(j) == 'A') j++;
+            if(name.charAt(i) == 'A' && j == size + 1) break;
+            int right = (i * 2) + (size - j);
+            int left = i + ((size - j) * 2);
+            leftRight = Math.min(Math.min(left, right), leftRight);
+		}
+        return upDown + leftRight;
+    }
+}
